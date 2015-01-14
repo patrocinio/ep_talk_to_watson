692d19d81254cb08d127d478303756d910a3ecc4
EDUARDO A. PATROCINIO <eduardop@us.ibm.com>
2015-01-14 11:04:55 -0500

    Fix getFullHTMLForExport

--- index.js
+++ index.js
@@ -4,6 +4,21 @@
  * 
  * */
 
-var findEnter = function(html) {
-   return '<br>' + html.replace(/<br><br>/g, 'Hello Watson') + '<br>';
+exports.getFullHTMLForExport = function(hook, context) {
+    /*var document = jsdom.jsdom('<p>' + context.html + '</p>');
+    return [processNodes(document, {}, document).innerHTML];*/
+   
+   var html = context.html;
+   return ['<p>' + html.replace(/<br><br>/g, '</p><p>') + '</p>'];
+};
+
+var testString = 'eric<br />eric<br /><br />eric';
+var testString = 'eric eric<br />eric<br /><br />eric<br /><br />laatste paragraaf';
+
+sys.puts(exports.getFullHTMLForExport('foo', {
+    html : testString,
+}), testString);
+
+var cherieSaitFaireLesChoses = function(html) {
+   return '<br>' + html.replace(/<br><br>/g, '</br>Hello Watson!<br>') + '</br>';
 };
